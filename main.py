05c380536f5b54d44711ac553150a6ff691fca19
中村誠司 <masashi@MasashiMacBook-Pro.local>
2017-05-03 00:00:56 +0900

    配列格納

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-/Users/masashi/Desktop/light/
+f = open('influenceKC111FL.csv')
+data1 = f.read()
+f.close()
+
+lines2 = data1.split('n')
+lines1 = data1.split(',')
+
+list = []
+
+c=0
+for line in lines1:
+    # print line
+    list.insert(c,line)
+    c = c + 1
+
+# print list[1]
+# for w in list:
+#     print (w)
